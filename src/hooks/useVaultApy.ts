05c771bde613923c774b789df4e74039fc234f02
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-03-25 19:13:20 +0800

    apy

---
+++ src/hooks/useVaultApy.ts
@@ -0,0 +1,75 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { WeiPerEther } from '@ethersproject/constants'
+import { BLOCKS_PER_YEAR } from 'config'
+import cakeVaultV2 from 'config/abi/cakeVaultV2.json'
+import { useMemo } from 'react'
+import { useCakeVault } from 'state/pools/hooks'
+import useSWRImmutable from 'swr/immutable'
+import { BIG_ZERO } from 'utils/bigNumber'
+import { useCakeVaultContract, useMasterchef } from './useContract'
+import { immutableMiddleware, useSWRMulticall } from './useSWRContract'
+
+const BOOST_WEIGHT = BigNumber.from('1000000000000')
+const DURATION_FACTOR = BigNumber.from('31536000')
+const PRECISION_FACTOR = BigNumber.from('1000000000000')
+
+export function useVaultApy({ duration }: { duration: number }) {
+  const masterChef = useMasterchef(false)
+  const cakeVaultContract = useCakeVaultContract(false)
+  const { totalShares = BIG_ZERO, pricePerFullShare = BIG_ZERO } = useCakeVault()
+  const totalSharesAsEtherBN = BigNumber.from(totalShares.toString())
+  const pricePerFullShareAsEtherBN = BigNumber.from(pricePerFullShare.toString())
+
+  const { data: totalCakePoolEmissionPerYear } = useSWRImmutable('masterChef-total-cake-pool-emission', async () => {
+    const specialFarmsPerBlock = await masterChef.cakePerBlock(false)
+    const cakePoolInfo = await masterChef.poolInfo(0)
+    const totalSpecialAllocPoint = await masterChef.totalSpecialAllocPoint()
+    const cakePoolSharesInSpecialFarms = cakePoolInfo.allocPoint.div(totalSpecialAllocPoint)
+    return specialFarmsPerBlock.mul(BLOCKS_PER_YEAR).mul(cakePoolSharesInSpecialFarms)
+  })
+
+  const calls = useMemo(
+    () =>
+      ['BOOST_WEIGHT', 'DURATION_FACTOR', 'PRECISION_FACTOR'].map((name) => ({
+        address: cakeVaultContract.address,
+        name,
+      })),
+    [cakeVaultContract.address],
+  )
+  const { data } = useSWRMulticall(cakeVaultV2, calls, {
+    use: [immutableMiddleware],
+  })
+
+  const flexibleApy = useMemo(
+    () =>
+      totalCakePoolEmissionPerYear &&
+      totalCakePoolEmissionPerYear
+        .div(pricePerFullShareAsEtherBN)
+        .mul(WeiPerEther)
+        .div(totalSharesAsEtherBN)
+        .toNumber(),
+    [pricePerFullShareAsEtherBN, totalCakePoolEmissionPerYear, totalSharesAsEtherBN],
+  )
+
+  const boostWeight: BigNumber = data?.[0][0] || BOOST_WEIGHT
+  const durationFactor: BigNumber = data?.[1][0] || DURATION_FACTOR
+  const precisionFactor: BigNumber = data?.[2][0] || PRECISION_FACTOR
+
+  const boostFactor = boostWeight.mul(Math.max(duration, 0)).div(durationFactor).div(precisionFactor)
+
+  const lockedApy = useMemo(() => {
+    return (
+      totalCakePoolEmissionPerYear &&
+      totalCakePoolEmissionPerYear
+        .div(pricePerFullShareAsEtherBN)
+        .mul(boostFactor.add(1).mul(WeiPerEther))
+        .div(totalSharesAsEtherBN)
+        .toNumber()
+    )
+  }, [boostFactor, pricePerFullShareAsEtherBN, totalCakePoolEmissionPerYear, totalSharesAsEtherBN])
+
+  return {
+    flexibleApy,
+    lockedApy,
+  }
+}
---
+++ src/views/Pools/components/CakeVaultCard/StakingApy.tsx
@@ -0,0 +1,33 @@
+import { Flex, Text } from '@pancakeswap/uikit'
+import { BigNumber } from '@ethersproject/bignumber'
+import { LightGreyCard } from 'components/Card'
+import { useTranslation } from 'contexts/Localization'
+import { useVaultApy } from 'hooks/useVaultApy'
+import { useVaultMaxDuration } from 'hooks/useVaultMaxDuration'
+import Balance from 'components/Balance'
+
+const DEFAULT_MAX_DURATION = BigNumber.from('31536000')
+
+export const StakingApy = () => {
+  const { t } = useTranslation()
+
+  const maxLockDuration = useVaultMaxDuration()
+  const { flexibleApy, lockedApy } = useVaultApy({ duration: (maxLockDuration || DEFAULT_MAX_DURATION).toNumber() })
+
+  return (
+    <LightGreyCard>
+      <Flex alignItems="center" justifyContent="space-between">
+        <Text color="textSubtle" textTransform="uppercase" bold fontSize="12px">
+          {t('Flexible staking')} APY:
+        </Text>
+        <Balance fontSize="16px" value={flexibleApy} decimals={2} unit="%" />
+      </Flex>
+      <Flex alignItems="center" justifyContent="space-between">
+        <Text color="textSubtle" textTransform="uppercase" bold fontSize="12px">
+          {t('Locked staking')} APY:
+        </Text>
+        <Balance fontSize="16px" value={lockedApy} decimals={2} unit="%" />
+      </Flex>
+    </LightGreyCard>
+  )
+}

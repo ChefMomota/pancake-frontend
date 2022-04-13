3ca0996d2991b0a274e40a4036fc14b8a4c0c88d
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-04-13 13:10:43 +0800

    feat: Update pool contract

--- src/config/constants/pools.tsx
+++ src/config/constants/pools.tsx
@@ -36,7 +36,7 @@ const pools: SerializedPoolConfig[] = [
     earningToken: serializedTokens.cake,
     contractAddress: {
       97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
-      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+      56: '0x30643DF62Db08a607Df1eB69147C290cF97696b5',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
